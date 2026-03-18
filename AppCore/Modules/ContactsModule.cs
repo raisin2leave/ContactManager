55897bd82b866f71a0b26e495e4eedaba706517b
ivanna.fedoseenko <ivanna.fedoseenko@microsoft.wsei.edu.pl>
2026-03-18 11:27:21 +0100

    failed lab 04

--- AppCore/Modules/ContactsModule.cs
+++ AppCore/Modules/ContactsModule.cs
@@ -1,10 +1,10 @@
-using System.Reflection;
-using FluentValidation;
-using FluentValidation.AspNetCore;
+using AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using AppCore.Validators;
 using AppCore.Mapper;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 namespace AppCore.Modules;
 
@@ -12,17 +12,23 @@ public static class ContactsModule
 {
     public static IServiceCollection AddContactsModule(this IServiceCollection services, IConfiguration configuration)
     {
+        // 1. Validators
         services.AddValidatorsFromAssemblyContaining<CreatePersonDtoValidator>();
         services.AddFluentValidationAutoValidation();
         
-        // Use the explicit static class call to avoid "Ambiguous invocation"
-        // This bypasses the extension method confusion.
-        var assemblies = new[] { typeof(ContactsMappingProfile).Assembly };
-        
-        // Try this specific syntax:
-        Microsoft.Extensions.DependencyInjection.ServiceCollectionExtensions
-            .AddAutoMapper(services, assemblies);
+        // 2. UNIVERSAL AUTOMAPPER REGISTRATION
+        // This is foolproof because it doesn't use the ambiguous DI extensions
+        var mapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<ContactsMappingProfile>();
+        });
 
+        // We create the mapper instance once
+        IMapper mapper = mapperConfig.CreateMapper();
+
+        // We register that specific instance as a Singleton
+        services.AddSingleton(mapper);
+        
         return services;
     }
 }
